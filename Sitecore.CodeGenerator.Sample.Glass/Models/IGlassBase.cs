cb41ccc0653f1cd8f28b7ec0092cecada6bc34de
Wesley Lomax <wesley@principlelogic.co.uk>
2017-10-03 11:48:11 +0100

    Updated output generated by the T4 template to include static classes, handled more Sitecore Field types and changes to rainbow provided.

---
+++ Sitecore.CodeGenerator.Sample.Glass/Models/IGlassBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Glass.Mapper.Sc.Configuration;
+using Glass.Mapper.Sc.Configuration.Attributes;
+using Sitecore.Data;
+using Sitecore.Globalization;
+
+namespace Sitecore.CodeGenerator.Sample.Glass.Models
+{
+    [SitecoreType(Cachable = true)]
+    public partial interface IGlassBase
+    {
+        [SitecoreId]
+        Guid Id { get; }
+
+        [SitecoreInfo(SitecoreInfoType.Language)]
+        Language Language { get; }
+
+        [SitecoreInfo(SitecoreInfoType.Name)]
+        string Name { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.TemplateId)]
+        ID TemplateId { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.TemplateName)]
+        string TemplateName { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.BaseTemplateIds)]
+        IEnumerable<ID> BaseTemplates { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.Url)]
+        string Url { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.Version)]
+        int Version { get; }
+    }
+}
